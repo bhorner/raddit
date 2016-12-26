0fe7c53e7a7cc99d82e309ec8b52e576ed078394
Brett Horner <hornerbrett@gmail.com>
2016-12-26 10:16:30 -0800

    Added User Migrate File

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224011809) do
+ActiveRecord::Schema.define(version: 20161226181057) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
